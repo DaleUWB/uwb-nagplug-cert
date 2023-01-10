3d9ec556db39b9fe014fbcb9f0167f3846012588
ahvakana@uw.edu <ahvakana@uw.edu>
2023-01-10 15:42:01 -0800

    changed dt/nm to skip when empty

--- Nagios-CertStatus.ps1
+++ Nagios-CertStatus.ps1
@@ -31,8 +31,8 @@ $certGood = @()
 
 # test certlist to see the status of each cert, recording subjectName and expiration date to pertinent array
 foreach($cert in $certlist) {
-    $cert.NotAfter ? ($dt = $cert.NotAfter.ToString()) : ($dt = $cert)
-    $cert.SubjectName.Name ? ($nm = $cert.SubjectName.Name.ToString()) : ($nm = $cert)
+    $cert.NotAfter ? ($dt = $cert.NotAfter.ToString()) : ( Continue )
+    $cert.SubjectName.Name ? ($nm = $cert.SubjectName.Name.ToString()) : ( Continue )
     
     if($cert.NotAfter -lt $today) {
         $certExpired += " $dt : $nm `n"
